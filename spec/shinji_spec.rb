99cf7bb956b74517d341717e4a971ffe75b6e513
Suguru Odai <ourqwers@gmail.com>
2014-01-04 01:04:55 +0900

    Add: 旧字ではない文字は変換しないことのテストケースを追加

--- spec/shinji_spec.rb
+++ spec/shinji_spec.rb
@@ -8,6 +8,11 @@ describe "String" do
         str = "稻飮隱營榮衞驛學廣"
         expect(str.shinji).to eq "稲飲隠営栄衛駅学広"
       end
+
+      it "旧字ではない文字は変換しないこと" do
+        str = "テスト東京大學"
+        expect(str.shinji).to eq "テスト東京大学"
+      end
     end
   end
 end
