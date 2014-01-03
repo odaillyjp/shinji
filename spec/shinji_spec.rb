89f56149fbbcfad33f74ed17c44f15c074cce153
Suguru Odai <ourqwers@gmail.com>
2014-01-04 01:03:43 +0900

    Fix: テストする文字列を修正

--- spec/shinji_spec.rb
+++ spec/shinji_spec.rb
@@ -5,8 +5,8 @@ describe "String" do
   describe "#shinji" do
     context "文字列に旧字が含まれるとき" do
       it "旧字を新字に変換した文字列を返すこと" do
-        str = "稻飮隱營榮衞驛"
-        expect(str.shinji).to eq "稲飲隠営栄衛駅"
+        str = "稻飮隱營榮衞驛學廣"
+        expect(str.shinji).to eq "稲飲隠営栄衛駅学広"
       end
     end
   end
