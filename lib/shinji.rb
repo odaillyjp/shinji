8222442f77112bc84a171b8e1cf3cacae3d0baf1
Suguru Odai <ourqwers@gmail.com>
2014-01-04 23:19:47 +0900

    Fix: config/shinji_list の読み出し処理を行うメソッドを実装して、リファクタリング

--- lib/shinji.rb
+++ lib/shinji.rb
@@ -14,37 +14,30 @@ end
 module Shinji
   CONFIG_DIR = File.expand_path('../../config', __FILE__)
 
+  @@shin_kanji = nil
+  @@kyu_kanji = nil
+
   module_function
 
   def to_shinji(str)
-    shin_kanji = nil
-    kyu_kanji = nil
+    load_shinji_list if @@shin_kanji.nil? && @@kyu_kanji.nil?
 
-    File.open("#{CONFIG_DIR}/shinji_list") do |file|
-      file_data = file.read.split("\n")
-      file_data.map! { |str| str.delete("\s").split(":") }
-      kyu_kanji, shin_kanji = file_data.transpose
-    end
-
-    shin_kanji = shin_kanji.join
-    kyu_kanji = kyu_kanji.join
-
-    str.tr(kyu_kanji, shin_kanji)
+    str.tr(@@kyu_kanji, @@shin_kanji)
   end
 
   def to_kyuji(str)
-    shin_kanji = nil
-    kyu_kanji = nil
+    load_shinji_list if @@shin_kanji.nil? && @@kyu_kanji.nil?
 
+    str.tr(@@shin_kanji, @@kyu_kanji)
+  end
+
+  def load_shinji_list
     File.open("#{CONFIG_DIR}/shinji_list") do |file|
       file_data = file.read.split("\n")
       file_data.map! { |str| str.delete("\s").split(":") }
       kyu_kanji, shin_kanji = file_data.transpose
+      @@shin_kanji = shin_kanji.join
+      @@kyu_kanji = kyu_kanji.join
     end
-
-    shin_kanji = shin_kanji.join
-    kyu_kanji = kyu_kanji.join
-
-    str.tr(shin_kanji, kyu_kanji)
   end
 end
