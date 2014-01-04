d25ffc74c955d818da8355d1a1ce48f57f9b85ca
Suguru Odai <ourqwers@gmail.com>
2014-01-04 23:28:11 +0900

    Add: コメントを追加

--- lib/shinji.rb
+++ lib/shinji.rb
@@ -12,7 +12,9 @@ class String
 end
 
 module Shinji
+  # 旧字・新字リストが置いてあるディレクトリのパス
   CONFIG_DIR = File.expand_path('../../config', __FILE__)
+  # 旧字・新字リストファイル名
   KANJI_LIST_FILE = 'kanji_list'
 
   @@shin_kanji = nil
@@ -23,22 +25,25 @@ module Shinji
   def to_shinji(str)
     load_shinji_list if @@shin_kanji.nil? && @@kyu_kanji.nil?
 
+    # 新字を旧字に変換する
     str.tr(@@kyu_kanji, @@shin_kanji)
   end
 
   def to_kyuji(str)
     load_shinji_list if @@shin_kanji.nil? && @@kyu_kanji.nil?
 
+    # 旧字を新字に変換する
     str.tr(@@shin_kanji, @@kyu_kanji)
   end
 
   def load_shinji_list
+    # 旧字・新字リストファイルの読み込み処理
     File.open("#{CONFIG_DIR}/#{KANJI_LIST_FILE}") do |file|
       file_data = file.read.split("\n")
       file_data.map! { |str| str.delete("\s").split(":") }
       kyu_kanji, shin_kanji = file_data.transpose
-      @@shin_kanji = shin_kanji.join
       @@kyu_kanji = kyu_kanji.join
+      @@shin_kanji = shin_kanji.join
     end
   end
 end
