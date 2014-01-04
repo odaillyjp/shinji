f2104fb684a06da173c3df1fe111f065f4935992
Suguru Odai <ourqwers@gmail.com>
2014-01-05 00:07:32 +0900

    Modify: 単語を統一するために、「旧字」を「旧漢字」に、「新字」を「新漢字」に変換

--- lib/shinji.rb
+++ lib/shinji.rb
@@ -12,9 +12,9 @@ class String
 end
 
 module Shinji
-  # 旧字・新字リストが置いてあるディレクトリのパス
+  # 旧漢字・新漢字リストが置いてあるディレクトリのパス
   CONFIG_DIR = File.expand_path('../../config', __FILE__)
-  # 旧字・新字リストファイル名
+  # 旧漢字・新漢字リストファイル名
   KANJI_LIST_FILE = 'kanji_list'
 
   @@shin_kanji = nil
@@ -25,19 +25,19 @@ module Shinji
   def to_shinji(str)
     load_kanji_list unless loaded_kanji_list?
 
-    # 新字を旧字に変換する
+    # 旧漢字を新漢字に変換する
     str.tr(@@kyu_kanji, @@shin_kanji)
   end
 
   def to_kyuji(str)
     load_kanji_list unless loaded_kanji_list?
 
-    # 旧字を新字に変換する
+    # 新漢字を旧漢字に変換する
     str.tr(@@shin_kanji, @@kyu_kanji)
   end
 
   def load_kanji_list
-    # 旧字・新字リストファイルの読み込み処理
+    # 旧漢字・新漢字リストファイルの読み込み処理
     File.open("#{CONFIG_DIR}/#{KANJI_LIST_FILE}") do |file|
       file_data = file.read.split("\n")
       file_data.map! { |str| str.delete("\s").split(":") }
