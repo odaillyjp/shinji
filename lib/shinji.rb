40f48b5c0dec03f8dae4c49ba10fc1418f1fe4cd
Suguru Odai <ourqwers@gmail.com>
2015-05-19 09:03:02 +0900

    Edit comments

--- lib/shinji.rb
+++ lib/shinji.rb
@@ -22,22 +22,34 @@ module Shinji
 
   module_function
 
+  # 旧漢字を新漢字に置換する
+  #
+  # Usage:
+  #
+  #   Shinji::to_kyuji('稻飮隱營榮衞驛學廣')
+  #   # => 稲飲隠営栄衛駅学広
+  #
   def to_shinji(str)
     load_kanji_list unless loaded_kanji_list?
 
-    # 旧漢字を新漢字に変換する
     str.tr(@@kyu_kanji, @@shin_kanji)
   end
 
+  # 新漢字を旧漢字に置換する
+  #
+  # Usage:
+  #
+  #   Shinji::to_kyuji('稲飲隠営栄衛駅学広')
+  #   # => 稻飮隱營榮衞驛學廣
+  #
   def to_kyuji(str)
     load_kanji_list unless loaded_kanji_list?
 
-    # 新漢字を旧漢字に変換する
     str.tr(@@shin_kanji, @@kyu_kanji)
   end
 
+  # 旧漢字・新漢字リストファイルの読み込みとメモ化を行う
   def load_kanji_list
-    # 旧漢字・新漢字リストファイルの読み込み処理
     File.open("#{CONFIG_DIR}/#{KANJI_LIST_FILE}") do |file|
       file_data = file.read.split("\n")
       file_data.map! { |str| str.delete("\s").split(":") }
