c5c596cae403d4c8e9fcf4680bd6dc312ae091b3
Suguru Odai <ourqwers@gmail.com>
2015-05-20 21:42:47 +0900

    Use single quote

--- lib/shinji.rb
+++ lib/shinji.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
-require "shinji/version"
+require 'shinji/version'
 
 class String
   def shinji
-    Shinji::to_shinji self
+    Shinji::to_shinji(self)
   end
 
   def kyuji
-    Shinji::to_kyuji self
+    Shinji::to_kyuji(self)
   end
 end
 
@@ -52,7 +52,7 @@ module Shinji
   def load_kanji_list
     File.open("#{CONFIG_DIR}/#{KANJI_LIST_FILE}") do |file|
       file_data = file.read.split("\n")
-      file_data.map! { |str| str.delete("\s").split(":") }
+      file_data.map! { |str| str.delete("\s").split(':') }
       @@kyu_kanji, @@shin_kanji = file_data.transpose.map(&:join)
     end
   end
